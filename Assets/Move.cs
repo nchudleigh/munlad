a3f53d8f89cbe8caeb948b0b6eb4066131517390
nchudleigh <nchudleigh@gmail.com>
2018-02-01 01:59:36 -0500

    Refactored Move.cs
    Moved all physics changes to FixedUpdate
    Additive Velocity Changes rather than absolute

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -11,6 +11,8 @@ public class Move : MonoBehaviour {
 
     [Range(0f, 3f)]
     public float dash_time = 1;
+    [Range(0f, 50f)]
+    public float dash_vel = 20;
 
     [Range(0f, 50f)]
     public float jump_velocity = 10;
@@ -33,7 +35,11 @@ public class Move : MonoBehaviour {
 
     // state
     bool jumping;
+    bool jump_request;
     bool hanging;
+    bool dashing;
+    // keeps remaining time of dash
+    float dash_ctr;
 
     // controls
     string hAxis;
@@ -80,11 +86,10 @@ public class Move : MonoBehaviour {
         center_y = boxCollider.center.y;
         bottom = center_y - (size_y / 2f);
         half_width = size_z / 2f;
-
+        // starting positions for Raycasts
         top_start = new Vector3(0, center_y + size_y / 2, 0);
         hang_start = top_start + new Vector3(0, -.85f, 0);
         bottom_start = new Vector3(0, center_y - size_y / 4, 0);
-
         ahead_start = new Vector3(0f, bottom + 0.1f, half_width - 0.1f);
         behind_start = new Vector3(0f, bottom + 0.1f, -half_width + 0.1f);
 
@@ -100,10 +105,9 @@ public class Move : MonoBehaviour {
       return Physics.Raycast(transform.position + position, direction, range);
     }
 
+    // Runs every Frame, input should be captured here
     void Update()
     {
-        float hMov = Input.GetAxis(hAxis);
-        float vMov = Input.GetAxis(vAxis);
         bool jump_btn = Input.GetButtonDown(jumpBtn);
         bool dash_btn = Input.GetButtonDown(dashBtn);
 
@@ -115,118 +119,106 @@ public class Move : MonoBehaviour {
         bool against_front_hang = isAgainst(hang_start, Vector3.forward, against_range, Color.grey);
         bool against_front_bottom = isAgainst(bottom_start, Vector3.forward, against_range, Color.green);
 
-        Vector3 deltaRun = new Vector3(0, 0, rb.velocity.z);
-        Vector3 deltaJump = new Vector3(0, rb.velocity.y, 0);
-        Vector3 deltaDash = Vector3.zero;
 
+        // DASH
 
-        // DASHING
-        if (dash_time <= 0f)
-        {
-            dash_time = 1f;
+        // Turn Dash On
+        if (dash_btn && !dashing && dash_ctr <= 0) {
+          dash_ctr = dash_time;
+          dashing = true;
+          trailRenderer.enabled = true;
+        }
+
+        if (dashing) {
+          // Decrement Dash Counter
+          dash_ctr -= Time.deltaTime;
+          // If out of time, turn Dash off
+          if (dash_ctr <= 0) {
+            dashing = false;
             trailRenderer.enabled = false;
+          }
         }
 
-        if (dash_btn && dash_time == 1f)
-        {
-            dash_time = dash_time - Time.deltaTime;
+        // JUMP
+        if (jump_btn && (grounded || hanging)) {
+          jump_request = true;
+          animator.SetBool("JUMP", true);
         }
 
-        if (dash_time > 0f && dash_time < 1f)
-        {
-
-            trailRenderer.enabled = true;
-            trailRenderer.time = dash_time * 0.6f;
-
-            //Move left
-            if (hMov < 0f)
-            {
-                deltaDash = new Vector3(0f, 0f, -10);
-
-            }
-            //Move right
-            if (hMov > 0f)
-            {
-                deltaDash = new Vector3(0f, 0f, 10);
-            }
-            if (!dash_btn)
-            {
-                dash_time = dash_time - Time.deltaTime;
-            }
+        if (grounded && jumping) {
+          jumping = false;
+          animator.SetBool("JUMP", false);
         }
-
-
-
-
-
-        // RUNNING
-        if (hMov != 0 )
-        {
-            deltaRun = new Vector3(0f, 0f, hMov * speed);
-
-            if (hMov < 0)
-            {
-                Quaternion target_back = Quaternion.Euler(0, 180f, 0);
-                transform.rotation = Quaternion.Slerp(transform.rotation, target_back, Time.deltaTime * speed);
-            }
-            else if (hMov > 0)
-            {
-                Quaternion target_forward = Quaternion.Euler(0, 0, 0);
-                transform.rotation = Quaternion.Slerp(transform.rotation, target_forward, Time.deltaTime * speed);
-
-            }
-        }
-
-
-
-        // JUMPING
-        // has landed again after jump
-        if (jumping && grounded && rb.velocity.y < .05 )
-        {
-            jumping = false;
-            animator.SetBool("JUMP", false);
-        }
-
-        // engage jump
-        if (jump_btn && (grounded || hanging))
-        {
-            deltaJump = Vector3.up * jump_velocity;
-            jumping = true;
-            animator.SetBool("JUMP", true);
-        }
-
-
-        Vector3 delta = deltaRun + deltaJump + deltaDash;
 
         // LEDGE HANG
         if (!grounded && !against_front_top && against_front_hang && !jump_btn)
         {
-            delta = Vector3.zero;
-            rb.useGravity = false;
             animator.SetBool("HANG", true);
             hanging = true;
         }
         else if (hanging)
         {
-            rb.useGravity = true;
             animator.SetBool("HANG", false);
             hanging = false;
         }
-
-
-        rb.velocity = delta;
-
 
         // update animator parameters
         animator.SetFloat("ZVEL", rb.velocity.z / speed, .1f, Time.deltaTime);
         animator.SetFloat("YVEL", rb.velocity.y);
     }
 
-    void FixedUpdate()
-    {
-        // on fall
-        if (rb.velocity.y < 0)
-        {
+
+    // Runs at the same clip no matter the frame rate / machine being run on
+    // Physics updates should be made here
+    void FixedUpdate() {
+        float hMov = Input.GetAxis(hAxis);
+        float vMov = Input.GetAxis(vAxis);
+
+        // RUN
+        if (hMov != 0) {
+          // which direction to apply running in
+          int sign = hMov > 0 ? 1 : -1;
+          // calculate required run speed
+          float delta_run_vel = speed * sign - rb.velocity.z;
+          // apply run speed
+          rb.velocity += Vector3.forward * delta_run_vel;
+          // rotate player to correct direction
+          Quaternion rotation_target = Quaternion.Euler(0, sign==1?0:180, 0);
+          transform.rotation = Quaternion.Slerp(transform.rotation, rotation_target, Time.deltaTime * speed);
+        }
+        // If there is no input make sure he comes to a complete stop
+        else if (Mathf.Abs(rb.velocity.z) < 0.05f) {
+          rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
+        }
+
+        // DASH
+        if (dashing) {
+          int sign = hMov > 0 ? 1 : -1;
+          // calculate required dash velocity
+          float delta_dash_vel = dash_vel * sign - rb.velocity.z;
+          // apply dash speed
+          rb.velocity += Vector3.forward * delta_dash_vel;
+          // rb.velocity += Vector3.Normalize(rb.velocity) * delta_dash_vel;
+          // rb.velocity += Vector3.Normalize(new Vector3(0f, vMov, hMov)) * delta_dash_vel;
+        }
+
+        // JUMP
+        if (jump_request) {
+          jumping = true;
+          rb.velocity += Vector3.up * jump_velocity;
+          jump_request = false;
+        }
+
+
+        if (hanging) {
+          rb.useGravity = false;
+          rb.velocity = Vector3.zero;
+        } else {
+          rb.useGravity = true;
+        }
+
+        // FALL
+        if (rb.velocity.y < 0) {
             // increase fall speed by gravitational pull * fall_multiplier
             rb.velocity += Vector3.up * Physics.gravity.y * fall_multiplier * Time.deltaTime;
         }
